9f0900d261c70ec1c9bb88c06bbecee4ec2c7f91
gbrllvini <gabvini2010@hotmail.com>
2024-08-13 17:43:09 -0300

    chore (Migrations): atualiza o banco de dados

---
+++ src/TestePraticoMvc/Migrations/202408132025462_AdicionaDACpfRg.cs
@@ -0,0 +1,20 @@
+﻿namespace TestePraticoMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdicionaDACpfRg : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Pessoas", "Cpf", c => c.String(nullable: false, maxLength: 11));
+            AlterColumn("dbo.Pessoas", "Rg", c => c.String(nullable: false, maxLength: 9));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Pessoas", "Rg", c => c.String());
+            AlterColumn("dbo.Pessoas", "Cpf", c => c.String());
+        }
+    }
+}
--- src/TestePraticoMvc/Models/Pessoa.cs
+++ src/TestePraticoMvc/Models/Pessoa.cs
@@ -16,19 +16,26 @@ namespace TestePraticoMvc.Models
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-        [StringLength(30, MinimumLength = 2, ErrorMessage = "o campo {0} precisa ter entre {1} e {2} caracteres.")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "o campo {0} precisa ter entre {2} e {1} caracteres.")]
         public string Sobrenome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [DataType(DataType.DateTime, ErrorMessage = "O campo {0} está em formato incorreto.")]
         [Display(Name = "Data de Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataNascimento { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         [StringLength(20, MinimumLength = 2, ErrorMessage = "Selecione uma opção válida para o campo {0}.")]
         [Display(Name = "Estado Civil")]
         public string EstadoCivil { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O campo {0} precisa ter {1} caracteres.")]
         public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "O campo {0} precisa ter {1} caracteres.")]
         public string Rg { get; set; }
     }
 }
