ccd714bebdccb232983df882662356226240e18e
gbrllvini <gabvini2010@hotmail.com>
2024-08-13 10:29:37 -0300

    first commit

---
+++ src/TestePraticoMvc/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestePraticoMvc.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+    }
+}
---
+++ src/TestePraticoMvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas através do seguinte
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("TestePraticoMvc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestePraticoMvc")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como falso torna não visíveis os tipos neste assembly
+// para componentes COM. Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("f617fd19-e672-45f1-9ace-2151fe9f557d")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Major Version
+//      Versão Secundária
+//      Build Number
+//      Revision
+//
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
